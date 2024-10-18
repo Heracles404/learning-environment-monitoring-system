2931fa47de21b0546ef8b77ae5b945ef12fca98e
rost-majestral <rj.escote5@gmail.com>
2024-10-19 00:22:51 +0800

    Initialize Record

--- client/dashboard/src/Data/Data.js
+++ client/dashboard/src/Data/Data.js
@@ -99,38 +99,14 @@ export const UpdatesData = [
   },
 ];
 
-// mock data Team
-export const mockDataTeam = [
-  {
-    id: 1,
-    name: "Jon Snow",
-    email: "jonsnow@gmail.com",
-    age: 35,
-    phone: "(665)121-5454",
-    access: "admin",
-  },
-  {
-    id: 2,
-    name: "Cersei Lannister",
-    email: "cerseilannister@gmail.com",
-    age: 42,
-    phone: "(421)314-2288",
-    access: "manager",
-  },
-  {
-    id: 3,
-    name: "Jaime Lannister",
-    email: "jaimelannister@gmail.com",
-    age: 45,
-    phone: "(422)982-6739",
-    access: "user",
-  },
-];
+
 
 // user data
 export const user = [
   {
     id: 1,
+
+    userID: 1,
     userName: 'SirNicanor',
     password: '123admin',
     role: 'Principal',
@@ -142,6 +118,8 @@ export const user = [
   },
   {
     id: 2,
+
+    userID: 2,
     userName: 'SirNicanor',
     password: '123admin',
     role: 'Physical Facilitator Coordinator',
@@ -154,6 +132,8 @@ export const user = [
   },
   {
     id: 3,
+
+    userID: 3,
     userName: 'SirNicanor',
     password: '123admin',
     role: 'SDRRM Coordinator',
@@ -164,6 +144,78 @@ export const user = [
     email: '3Reyes@gmail.com'
   },
 ];
+
+export const readout = [
+  {
+    id: 1,
+
+    readoutID: 1,
+    date: '10/16/2024',
+    time: '11:59:00 PM',
+    temperature: "1",
+    humidity: "1",
+
+    heatIndex: "1",
+    lighting: "1",
+    headCount: "1",
+    oxygen: "1",
+    carbonDioxide: "1",
+    sulfurDioxide: "1",
+    particulateMatter: "1",
+    // set conditions in arduino
+    indoorAir: "Good",
+    outdoorAir: "Good",
+    temp: "Good",
+    remarks: "Good",
+
+  },
+  {
+    id: 2,
+
+    readoutID: 2,
+    date: '10/16/2024',
+    time: '11:59:00 PM',
+    temperature: "2",
+    humidity: "2",
+
+    heatIndex: "2",
+    lighting: "2",
+    headCount: "2",
+    oxygen: "2",
+    carbonDioxide: "2",
+    sulfurDioxide: "2",
+    particulateMatter: "2",
+    // set conditions in arduino
+    indoorAir: "Good",
+    outdoorAir: "Good",
+    temp: "Good",
+    remarks: "Good",
+
+  },
+  {
+    id: 3,
+
+    readoutID: 3,
+    date: '10/16/2024',
+    time: '11:59:00 PM',
+    temperature: "3",
+    humidity: "3",
+
+    heatIndex: "3",
+    lighting: "3",
+    headCount: "3",
+    oxygen: "3",
+    carbonDioxide: "3",
+    sulfurDioxide: "3",
+    particulateMatter: "3",
+    // set conditions in arduino
+    indoorAir: "Good",
+    outdoorAir: "Good",
+    temp: "Good",
+    remarks: "Good",
+
+  },
+]
 
 
 //Sidebar Data
@@ -180,7 +232,7 @@ export const SidebarData =[
     },
     {
         icon: UilClipboardAlt,
-        heading: "Reports",
+        heading: "Records",
         path: "/records"
 
     },
--- client/dashboard/src/pages/Records/Records.jsx
+++ client/dashboard/src/pages/Records/Records.jsx
@@ -1,13 +1,155 @@
+import { Box, Typography, colors } from "@mui/material";
 import React from 'react'
+import { DataGrid } from "@mui/x-data-grid";
+import { readout } from '../../Data/Data'
+import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
+import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
+import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
+// import Header from "../../components/Header";
 
 const Records = () => {
-  return (
-    <div>
-        <h1>Records</h1>
-        readID - date - time - temp - humidity- lighting - headCount - 
-        oxygen - carbonDioxide - sulfur Dioxide - particulatematter
-    </div>
-  )
-}
+  // const theme = useTheme();
+  // const colors = tokens(theme.palette.mode);
+  const columns = [
+    { field: "id", 
+      headerName: "ID",
+      flex: .2,
+    },
+    {
+      field: "date",
+      headerName: "Date",
+      flex: 2,
+    },
+    {
+      field: "time",
+      headerName: "Time",
+      flex: 2,
+    },
+    // Monitors
+    {
+      field: "temperature",
+      headerName: "Temperature",
+      flex: 2,
+      cellClassName: "role-column--cell", 
+    },
+    {
+      field: "humidity",
+      headerName: "humidity",
+      flex: 2,
+      cellClassName: "role-column--cell", 
+    },
+    {
+      field: "heatIndex",
+      headerName: "heatIndex",
+      flex: 2,
+      cellClassName: "role-column--cell", 
+    },
+    {
+      field: "lighting",
+      headerName: "lighting",
+      flex: 2,
+      cellClassName: "role-column--cell", 
+    },
+    {
+      field: "headCount",
+      headerName: "headCount",
+      flex: 2,
+      cellClassName: "role-column--cell", 
+    },
+    {
+      field: "oxygen",
+      headerName: "oxygen",
+      flex: 2,
+      cellClassName: "role-column--cell", 
+    },
+    {
+      field: "carbonDioxide",
+      headerName: "carbonDioxide",
+      flex: 2,
+      cellClassName: "role-column--cell", 
+    },
+    {
+      field: "sulfurDioxide",
+      headerName: "sulfurDioxide",
+      flex: 2,
+      cellClassName: "role-column--cell", 
+    },
+    {
+      field: "particulateMatter",
+      headerName: "particulateMatter",
+      flex: 2,
+      cellClassName: "role-column--cell", 
+    },
+    // set conditions in arduino
+    {
+      field: "indoorAir",
+      headerName: "indoorAir",
+      flex: 2,
+      cellClassName: "role-column--cell", 
+    },
+    {
+      field: "outdoorAir",
+      headerName: "outdoorAir",
+      flex: 2,
+      cellClassName: "role-column--cell", 
+    },
+    {
+      field: "temp",
+      headerName: "temp",
+      flex: 2,
+      cellClassName: "role-column--cell", 
+    },
+    {
+      field: "remarks",
+      headerName: "remarks",
+      flex: 2,
+      cellClassName: "role-column--cell", 
+    },
 
-export default Records
+  ];
+
+  return (
+    <Box m="20px">
+      <div>
+        <h1>Records</h1>
+        <h3>Managing the Records</h3>
+      </div>
+      <Box
+        m="40px 0 0 0"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          // "& .name-column--cell": {
+          //   color: 'red',
+          // },
+          "& .role-column--cell": {
+            color: 'orange',
+          },
+          "& .MuiDataGrid-columnHeader": {
+            backgroundColor: '#ffe1bc',
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: 'white',
+          },
+          "& .MuiDataGrid-footerContainer": {
+            borderTop: "none",
+            backgroundColor: '#ffe1bc',
+          },
+          "& .MuiCheckbox-root": {
+            color: '#00F5B4',
+          },
+        }}
+      >
+        <DataGrid rows={readout} columns={columns} />
+      </Box>
+    </Box>
+  );
+};
+
+export default Records;
