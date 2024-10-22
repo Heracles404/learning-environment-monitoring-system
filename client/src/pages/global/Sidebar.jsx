17095f9871d0b45ea2cc011eb87e8045bd15bdd6
rost-majestral <rj.escote5@gmail.com>
2024-10-22 16:51:41 +0800

    Updated Dashboard

--- client/src/pages/global/Sidebar.jsx
+++ client/src/pages/global/Sidebar.jsx
@@ -126,13 +126,13 @@ const Sidebar = () => {
               selected={selected}
               setSelected={setSelected}
             />
-            <Item
+            {/* <Item
               title="Calendar"
               to="/calendar"
               icon={<CalendarTodayOutlinedIcon />}
               selected={selected}
               setSelected={setSelected}
-            />
+            /> */}
 
             <Typography
               variant="h6"
