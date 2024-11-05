15702a6b7417cf23f2dc459604ee11374b5ef894
yuuaaannnn <159892828+yuuaaannnn@users.noreply.github.com>
2024-11-05 15:25:24 +0800

    updated

--- client/src/hooks/sensors.requests.js
+++ client/src/hooks/sensors.requests.js
@@ -1,29 +1,27 @@
+// sensors.requests.js
 const API_URL = 'http://localhost:8000';
 
-
-async function httpGetAllReadouts(){
-    const response = await fetch(`${API_URL}/sensors`);
+async function httpGetAllReadouts() {
+    const response = await fetch(`${API_URL}/sensors.requests`);
     return await response.json();
 }
 
-// nag cconflict sa hardcoded "id" kaya cinomment ko muna
-// async function httpGetReadoutById(_id){
-//     const response = await fetch(`${API_URL}/sensors/${id}`);
-//     return await response.json();
-// } 
+async function httpGetReadoutById(_id) {
+    const response = await fetch(`${API_URL}/sensors/${_id}`);
+    return await response.json();
+}
 
-async function httpGetReadoutsByDate(startDate, endDate){
+async function httpGetReadoutsByDate(startDate, endDate) {
     const response = await fetch(`${API_URL}/sensors/date/date?startDate=${startDate}&endDate=${endDate}`);
     return await response.json();
 }
 
-async function httpGetReadoutsByTime(time){
+async function httpGetReadoutsByTime(time) {
     const response = await fetch(`${API_URL}/sensors/time/${time}`);
     return await response.json();
 }
 
-
-async function httpNewReadouts(readout){
+async function httpNewReadouts(readout) {
     try {
         return await fetch(`${API_URL}/sensors`, {
             method: "POST",
@@ -37,25 +35,9 @@ async function httpNewReadouts(readout){
             ok: false,
         };
     }
-
-    // Send Data Body
-    //     "temperature": 6,
-    //     "humidity": 33,
-    //     "heatIndex": 37,
-    //     "lighting": 160,
-    //     "headCount": 54,
-    //     "oxygen": 24,
-    //     "carbonDioxide": 600,
-    //     "sulfurDioxide": 30,
-    //     "particulateMatter": 15,
-    //     "indoorAir": "Good",
-    //     "outdoorAir": "Good",
-    //     "temp": "Good",
-    //     "remarks": "Good"
 }
 
-
-async function httpDeleteReadout(_id){
+async function httpDeleteReadout(_id) {
     try {
         return await fetch(`${API_URL}/sensors/${_id}`, {
             method: "DELETE",
@@ -67,78 +49,8 @@ async function httpDeleteReadout(_id){
         };
     }
 }
-export const readout = [
-    {
-      id: 1,
-  
-      readoutID: 1,
-      date: '10/16/2024',
-      time: '11:59:00 PM',
-      temperature: "1",
-      humidity: "1",
-  
-      heatIndex: "1",
-      lighting: "1",
-      headCount: "1",
-      oxygen: "1",
-      carbonDioxide: "1",
-      sulfurDioxide: "1",
-      particulateMatter: "1",
-      // set conditions in arduino
-      indoorAir: "Good",
-      outdoorAir: "Good",
-      temp: "Good",
-      remarks: "Good",
-  
-    },
-    {
-      id: 2,
-  
-      readoutID: 2,
-      date: '10/16/2024',
-      time: '11:59:00 PM',
-      temperature: "2",
-      humidity: "2",
-  
-      heatIndex: "2",
-      lighting: "2",
-      headCount: "2",
-      oxygen: "2",
-      carbonDioxide: "2",
-      sulfurDioxide: "2",
-      particulateMatter: "2",
-      // set conditions in arduino
-      indoorAir: "Good",
-      outdoorAir: "Good",
-      temp: "Good",
-      remarks: "Good",
-  
-    },
-    {
-      id: 3,
-  
-      readoutID: 3,
-      date: '10/16/2024',
-      time: '11:59:00 PM',
-      temperature: "3",
-      humidity: "3",
-  
-      heatIndex: "3",
-      lighting: "3",
-      headCount: "3",
-      oxygen: "3",
-      carbonDioxide: "3",
-      sulfurDioxide: "3",
-      particulateMatter: "3",
-      // set conditions in arduino
-      indoorAir: "Good",
-      outdoorAir: "Good",
-      temp: "Good",
-      remarks: "Good",
-  
-    },
-  ]
-async function httpDeleteAllReadouts(){
+
+async function httpDeleteAllReadouts() {
     try {
         return await fetch(`${API_URL}/sensors`, {
             method: "DELETE",
@@ -151,3 +63,13 @@ async function httpDeleteAllReadouts(){
     }
 }
 
+// Export all functions
+module.exports = {
+    httpGetAllReadouts,
+    httpGetReadoutById,
+    httpGetReadoutsByDate,
+    httpGetReadoutsByTime,
+    httpNewReadouts,
+    httpDeleteReadout,
+    httpDeleteAllReadouts,
+};
--- server/src/app.js
+++ server/src/app.js
@@ -2,8 +2,8 @@ const path = require('path');
 const express = require('express');
 const cors = require('cors');
 
-const usersRouter = require('./routes/users/users.router')
-const sensorsRouter = require('./routes/sensors/sensors.router')
+const usersRouter = require('./routes/users/users.router');
+const sensorsRouter = require('./routes/sensors/sensors.router');
 
 const app = express();
 
@@ -11,21 +11,29 @@ app.use(cors({
     origin: "http://localhost:3000",
 }));
 
-const publicPath = path.resolve(__dirname, "..", "..", "client", "build");
-
-app.use(express.static(publicPath));
-
 app.use(express.json());
 
 app.use('/users', usersRouter);
 app.use('/sensors', sensorsRouter);
 
+// Add this route to handle httpGetAllReadouts
+app.get('/httpGetAllReadouts', async (req, res) => {
+    try {
+        const fetch = (await import('node-fetch')).default;
+        const response = await fetch('http://localhost:8000/sensors');
+        const data = await response.json();
+        res.json(data);
+    } catch (err) {
+        res.status(500).json({ error: 'Failed to fetch readouts' });
+    }
+});
+
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
-})
+    res.sendFile(path.join(publicPath, 'index.html'), (err) => {
+        if (err) {
+            res.status(404).send('index.html not found');
+        }
+    });
+});
 
 module.exports = app;
-
-
-
-
