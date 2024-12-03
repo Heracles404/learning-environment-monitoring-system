d9609f0da7e8a462a8f4eb9e6e118e7eb794bdcb
Nazareno Bianca <103508458+Heracles404@users.noreply.github.com>
2024-12-03 12:07:37 +0800

    BH1750
    Diagrams, wiring-schematic, init code

--- hardware/BH1750/BH1750.ino
+++ hardware/BH1750/BH1750.ino
@@ -1,9 +1,27 @@
-void setup() {
-  // put your setup code here, to run once:
+// https://randomnerdtutorials.com/arduino-bh1750-ambient-light-sensor/
 
+#include <Wire.h>
+#include <BH1750.h>
+
+BH1750 lightMeter;
+
+void setup(){
+  Serial.begin(9600);
+
+  // Initialize the I2C bus (BH1750 library doesn't do this automatically)
+  Wire.begin();
+  // On esp8266 you can select SCL and SDA pins using Wire.begin(D4, D3);
+  // For Wemos / Lolin D1 Mini Pro and the Ambient Light shield use Wire.begin(D2, D1);
+
+  lightMeter.begin();
+
+  Serial.println(F("BH1750 Test begin"));
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  float lux = lightMeter.readLightLevel();
+  Serial.print("Light: ");
+  Serial.print(lux);
+  Serial.println(" lx");
+  delay(5000);
 }
