4ce0b883df8e8a5f231f32b981db63a4591e6ee9
rost-majestral <rj.escote5@gmail.com>
2024-12-06 08:53:12 +0800

    Initialized Device Status

--- client/src/pages/dashboard/index.jsx
+++ client/src/pages/dashboard/index.jsx
@@ -26,6 +26,8 @@ import DBRecords from "../../components/DashboardTables/DBRecords";
 import DBAccounts from "../../components/DashboardTables/DBAccounts";
 
 import DevicesIcon from '@mui/icons-material/Devices';
+import RssFeedOutlinedIcon from '@mui/icons-material/RssFeedOutlined';
+import WarningAmberOutlinedIcon from '@mui/icons-material/WarningAmberOutlined';
 
 import OxygenChart from "../../components/LineCharts/Oxygen";
 import CarbonDioxideChart from "../../components/LineCharts/CarbonDioxide";
@@ -43,10 +45,98 @@ const Dashboard = () => {
       {/* HEADER */}
       <Box 
       display="flex" 
-      // justifyContent="space-between"
-       alignItems="center">
+      justifyContent="space-between"
+       alignItems="space-between">
         <Header title="DASHBOARD" subtitle="Welcome to Edilberto S. Legaspi Integrated High School
         " />
+
+        {/* STATUS */}
+    <Grid container 
+    display='flex'
+    justifyContent ="space-between"
+    alignContent="space-between"
+    pr="120px"
+    ml="4px"
+    rowSpacing={1}
+    columnSpacing={{ xs: 1, sm: 3, md: 3 }}
+    // pr="50px"
+    mb="30px">
+      <Grid item size={{ xs: 6, sm: 4, md: 4, lg: 2 }}>
+        <Box
+          // gridColumn="span 2"        
+          backgroundColor={colors.greenAccent[600]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          sx={{
+            borderRadius: '12px',
+            height: '62px', 
+            width: {
+              xs: 136, // 0
+              sm: 110, // 600
+              md: 136, // 900
+              lg: 136, // 1200
+              xl: 136, // 1536
+          },
+          }}
+        >
+          <StatBox
+            title="ACTIVE"
+            subtitle="Device 1"
+            // progress="0.75"
+            // increase="+14%"
+            // icon={
+            //   <DevicesIcon
+            //     sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+            //   />
+            // }
+            icon2={
+              <RssFeedOutlinedIcon 
+                sx={{ color: "#00cc00 " , fontSize: "46px" }}
+              />
+            }
+          />
+        </Box>
+        </Grid>
+        <Grid item size={{ xs: 6, sm: 4, md: 4, lg: 2 }}>
+        <Box
+          // gridColumn="span 2"
+          backgroundColor={colors.greenAccent[600]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          sx={{
+            borderRadius: '12px',
+            height: '62px', 
+            width: {
+              xs: 136, // 0
+              sm: 110, // 600
+              md: 136, // 900
+              lg: 136, // 1200
+              xl: 136, // 1536
+          },
+          }}
+
+        >
+          <StatBox
+            title="ALERT"
+            subtitle="Device 2"
+            // progress="0.50"
+            // increase="+21%"
+            // icon={
+            //   <DevicesIcon
+            //     sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+            //   />
+            // }
+            icon2={
+              <WarningAmberOutlinedIcon
+                sx={{ color: colors.redAccent[600], fontSize: "46px" }}
+              />
+            }
+          />
+        </Box>
+        </Grid>
+      </Grid>
 
         {/* <Box>
           <Button
@@ -70,142 +160,7 @@ const Dashboard = () => {
       <DBRecords/>
       <DBAccounts/>
       
-       {/* STATUS */}
-    <Grid container 
-    display='flex'
-    justifyContent ="center"
-    alignContent="space-between"
-    rowSpacing={1}
-    columnSpacing={{ xs: 1, sm: 3, md: 3 }}
-    // pr="50px"
-    mb="30px">
-      <Grid item size={{ xs: 6, sm: 4, md: 4, lg: 2 }}>
-        <Box
-          // gridColumn="span 2"        
-          backgroundColor={colors.primary[400]}
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-          sx={{
-            height: '112px', 
-            width: {
-              xs: 136, // 0
-              sm: 110, // 600
-              md: 136, // 900
-              lg: 156, // 1200
-              xl: 136, // 1536
-          },
-          }}
-        >
-          <StatBox
-            title="ACTIVE"
-            subtitle="Device 1"
-            progress="0.75"
-            // increase="+14%"
-            icon={
-              <DevicesIcon
-                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
-              />
-            }
-          />
-        </Box>
-        </Grid>
-        <Grid item size={{ xs: 6, sm: 4, md: 4, lg: 2 }}>
-        <Box
-          // gridColumn="span 2"
-          backgroundColor={colors.primary[400]}
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-          sx={{height: '112px', 
-            width: {
-              xs: 136, // 0
-              sm: 110, // 600
-              md: 136, // 900
-              lg: 136, // 1200
-              xl: 136, // 1536
-          },
-          }}
-
-        >
-          <StatBox
-            title="ACTIVE"
-            subtitle="Device 2"
-            progress="0.50"
-            // increase="+21%"
-            icon={
-              <DevicesIcon
-                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
-              />
-            }
-          />
-        </Box>
-        </Grid>
-        {/* <Grid item size={{ xs: 6, sm: 4, md: 4, lg: 2 }}>
-        <Box
-          // gridColumn="span 2"
-          backgroundColor={colors.primary[400]}
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-          sx={{height: '112px', 
-            width: {
-              xs: 136, // 0
-              sm: 110, // 600
-              md: 136, // 900
-              lg: 136, // 1200
-              xl: 136, // 1536
-          },
-          }}
-
-        >
-          <StatBox
-            title="GOOD"
-            subtitle="Oxygen"
-            progress="0.30"
-            // increase="+5%"
-            icon={
-              <AirIcon
-                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
-              />
-            }
-          />
-        </Box>
-        </Grid>
-        <Grid item size={{ xs: 6, sm: 4, md: 4, lg: 2 }}>
-        <Box
-          // gridColumn="span 2"
-          backgroundColor={colors.primary[400]}
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-          sx={{height: '112px', 
-            width: {
-              xs: 136, // 0
-              sm: 110, // 600
-              md: 136, // 900
-              lg: 136, // 1200
-              xl: 136, // 1536
-          },
-          }}
-
-        >
-          <StatBox
-            title="BAD"
-            subtitle="Carbon Dioxide"
-            progress="0.80"
-            // increase="+42%"
-            icon={
-              <Co2Icon
-                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
-              />
-            }
-          />
-        </Box>
-        </Grid> */}
-        
-      
-      </Grid>
+       
     </Box>
   );
 };
