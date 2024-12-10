e56fd6e0933db19959853e7db16a53d0de3ea5b0
Nazareno Bianca <103508458+Heracles404@users.noreply.github.com>
2024-12-10 19:59:50 +0800

    api save

--- server/src/app.js
+++ server/src/app.js
@@ -9,7 +9,7 @@ const vogRouter = require('./routes/vog/vog.router');
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000"],
 }));
 
 app.use(express.json());
