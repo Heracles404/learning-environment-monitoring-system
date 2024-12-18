3aff51a331bf2521c4b1fb9b9c17114810833e21
yuuaaannnn <159892828+yuuaaannnn@users.noreply.github.com>
2024-12-18 13:05:45 +0800

    Update Width (Login)

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,36 +1,34 @@
 import React, { useState } from 'react';
-import { Box, Typography, TextField, Button, Link, Grid } from '@mui/material';
+import { Box, Typography, TextField, Button, Grid } from '@mui/material';
 import { useNavigate } from "react-router-dom";
 import backgroundImage from '../../imgs/ESLIHS_BG.png';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import LockIcon from '@mui/icons-material/Lock';
 
-import {httpAuthenticateUser} from "../../hooks/users.requests";
+import { httpAuthenticateUser } from "../../hooks/users.requests";
 
-    const LoginPage = () => {
-        const [username, setUsername] = useState("");
-        const [password, setPassword] = useState("");
-        // const [users, setUsers] = useState([]); // State to hold user data
-        const navigate = useNavigate();
+const LoginPage = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
 
+    const handleLogin = async (e) => {
+        e.preventDefault();
 
-        const handleLogin = async (e) => {
-            e.preventDefault();
+        try {
+            const userData = await httpAuthenticateUser(username, password); // Call the authenticate function with username and password
 
-            try {
-                const userData = await httpAuthenticateUser (username, password); // Call the authenticate function with username and password
-
-                if (userData.ok === false) {
-                    alert(userData.error || "Invalid credentials"); // Show error message if authentication fails
-                } else {
-                    localStorage.setItem("auth", "true");
-                    navigate("/dashboard"); // Navigate to the dashboard on successful authentication
-                }
-            } catch (error) {
-                console.error("Authentication error:", error);
-                alert("Authentication failed. Please try again later.");
+            if (userData.ok === false) {
+                alert(userData.error || "Invalid credentials"); // Show error message if authentication fails
+            } else {
+                localStorage.setItem("auth", "true");
+                navigate("/dashboard"); // Navigate to the dashboard on successful authentication
             }
-        };
+        } catch (error) {
+            console.error("Authentication error:", error);
+            alert("Authentication failed. Please try again later.");
+        }
+    };
 
     return (
         <Box
@@ -51,21 +49,21 @@ import {httpAuthenticateUser} from "../../hooks/users.requests";
                 container 
                 spacing={2} 
                 sx={{
-                    width: { xs: '100%', md: '80%', lg: '60%' }, 
+                    width: { xs: '100%', sm: '100%', md: '80%' }, 
                     mx: 'auto',
                 }}
             >
                 {/* Welcome Box */}
-                <Grid item xs={12} md={4} display="flex" justifyContent="center" alignItems="center">
+                <Grid item xs={12} sm={12} md={4} display="flex" justifyContent="center" alignItems="center">
                     <Box
                         sx={{
                             backgroundImage: 'linear-gradient(to top, #b7ebde, #94e2cd, #4cceac, #2a9d8f, #1F705A)',
                             color: 'white',
                             height: '100%',
-                            maxWidth: { xs: '100%', sm: '80%', md: '100%' },
-                            padding: '16px',
-                            borderRadius: '15px',
-                            textAlign: 'center'
+                            padding: { xs: '30px', sm: '50px' },
+                            borderRadius: '20px',
+                            textAlign: 'center',
+                            width: '100%'
                         }}
                     >
                         <Typography variant='h5' fontWeight="bold" mb={2}>
@@ -82,21 +80,25 @@ import {httpAuthenticateUser} from "../../hooks/users.requests";
                 </Grid>
 
                 {/* Login Form */}
-                <Grid item xs={12} md={4} display="flex" justifyContent="center" alignItems="center">
+                <Grid item xs={12} sm={12} md={4} display="flex" justifyContent="center" alignItems="center">
                     <Box
                         sx={{
                             backgroundImage: 'linear-gradient(to top, #b7ebde, #94e2cd, #4cceac, #4cceac, #2a9d8f, #1F705A)',
                             color: 'white',
                             height: '100%',
-                            maxWidth: { xs: '100%', sm: '80%', md: '100%' },
                             padding: '16px',
                             borderRadius: '15px',
+                            display: 'flex',
+                            flexDirection: 'column',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            width: '100%'
                         }}
                     >
                         <Typography variant="h6" fontWeight="bold" textAlign="center" mb={2}>
                             Login to ESLIHS - Monitoring System
                         </Typography>
-                        <form onSubmit={handleLogin}>
+                        <form onSubmit={handleLogin} style={{ width: '100%' }}>
                             <Box mb={2} sx={{ display: 'flex', alignItems: 'center' }}>
                                 <AccountCircle sx={{ fontSize: 38, color: 'action.active', mr: 1 }} />
                                 <TextField
@@ -135,7 +137,7 @@ import {httpAuthenticateUser} from "../../hooks/users.requests";
                             <Button type="submit" variant="contained" fullWidth sx={{
                                 backgroundColor: '#36D4FC', 
                                 height: '50px',  
-                                borderRadius: '15px', 
+                                borderRadius: '25px', 
                                 fontWeight: 'bold',
                                 mt: 2
                             }}>
@@ -146,16 +148,20 @@ import {httpAuthenticateUser} from "../../hooks/users.requests";
                 </Grid>
 
                 {/* Links Box */}
-                <Grid item xs={12} md={4} display="flex" justifyContent="center" alignItems="center">
+                <Grid item xs={12} sm={12} md={4} display="flex" justifyContent="center" alignItems="center">
                     <Box
                         sx={{
                             backgroundImage: 'linear-gradient(to top, #b7ebde, #94e2cd, #4cceac, #2a9d8f, #1F705A)',
                             color: 'white',
                             height: '100%',
-                            maxWidth: { xs: '100%', sm: '80%', md: '100%' },
                             padding: '16px',
                             borderRadius: '15px',
-                            textAlign: 'center'
+                            textAlign: 'center',
+                            display: 'flex',
+                            flexDirection: 'column',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            width: '100%'
                         }}
                     >
                         <Typography variant="h6" fontWeight="bold" gutterBottom>
@@ -170,7 +176,7 @@ import {httpAuthenticateUser} from "../../hooks/users.requests";
                             target="_blank"
                             rel="noopener"
                             fullWidth
-                            sx={{ backgroundColor: '#FF3333', borderRadius: '15px', mb: 2 }}
+                            sx={{ backgroundColor: '#FF3333', borderRadius: '25px', mb: 2 }}
                         >
                             ESLIHS Facebook PAGE
                         </Button>
@@ -180,7 +186,7 @@ import {httpAuthenticateUser} from "../../hooks/users.requests";
                             target="_blank"
                             rel="noopener"
                             fullWidth
-                            sx={{ backgroundColor: '#FF3333', borderRadius: '15px' }}
+                            sx={{ backgroundColor: '#FF3333', borderRadius: '25px' }}
                         >
                             Dep-ED PAGE
                         </Button>
