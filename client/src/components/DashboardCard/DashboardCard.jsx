97b8bb422b2c3b8347274bf7742865797d738552
rost-majestral <rj.escote5@gmail.com>
2024-12-18 16:04:45 +0800

    Modified Cards Information (Logo + Styles)

--- client/src/components/DashboardCard/DashboardCard.jsx
+++ client/src/components/DashboardCard/DashboardCard.jsx
@@ -42,7 +42,7 @@ function DBCompactCard({ param, setExpanded }) {
         <span>{param.title}</span>
       </div>
       <div className="detail">
-        <Png />
+        <Png style={{ width: '50px', height: '50px' }}/>
         <span>{param.value}</span>
         <span>Last 24 hours</span>
       </div>
@@ -117,7 +117,7 @@ function DBExpandedCard({ param, setExpanded }) {
       <div className="DBchartContainer">
         <Chart options={data.options} series={param.series} type="area" />
       </div>
-      <span>Last 24 hours</span>
+      {/* <span>Last 24 hours</span> */}
     </motion.div>
   );
 }
