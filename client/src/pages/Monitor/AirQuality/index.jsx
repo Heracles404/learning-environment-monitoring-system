7d2098b93282a0f94a636351ca2712e74021f55d
rost-majestral <rj.escote5@gmail.com>
2025-01-15 06:14:24 +0800

    Modified Air Quality Monitor Page (with Table + Ideal info)

--- client/src/pages/Monitor/AirQuality/index.jsx
+++ client/src/pages/Monitor/AirQuality/index.jsx
@@ -8,7 +8,15 @@ import Co2Icon from '@mui/icons-material/Co2';
 
 import CO2Cards from "../../../components/ChartCards/AirQuality/CO2Cards";
 import ExpandedCard from "../../../components/ChartCards/AirQuality/CO2Cards";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import DBRecords from "../../../components/DashboardTables/DBRecords";
+import AirQualityRecordTable from "../../../components/RecordTables/AirQualityRecordTable";
 const CarbonDioxideMonitor = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
   return (
         <Box m="0 5px 0 5px" height="100vh" overflow="auto">
       {/* HEADER */}
@@ -20,7 +28,35 @@ const CarbonDioxideMonitor = () => {
        >
         <Header title="Air Quality" subtitle="Monitoring the Air Quality" />
       </Box>
+      <Box>
+        
+      
+      <Box 
+            display="flex" 
+            // justifyContent="space-between"
+            //  alignItems="space-between"
+             sx={{
+              flexDirection: { xs: 'column', sm: 'row', md: "column", lg: "row" }
+            }}>
+      {/* <DBRecords/> */}
+      <AirQualityRecordTable/>
+        <Accordion defaultExpanded>
+          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography color={colors.greenAccent[500]} variant="h5">
+              Ideal Range for IAQ
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Lorem .
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+        </Box>
       <CO2Cards/>
+      </Box>
+      
+      
     </Box> 
   )
 }
