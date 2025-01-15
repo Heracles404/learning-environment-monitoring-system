d9f532367630498953556ad95154a474ba423612
rost-majestral <rj.escote5@gmail.com>
2025-01-15 18:17:39 +0800

    Initialized Chart + Table + TextHolder Design (Monitor Page)

--- client/src/pages/Monitor/AirQuality/index.jsx
+++ client/src/pages/Monitor/AirQuality/index.jsx
@@ -18,35 +18,55 @@ const CarbonDioxideMonitor = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   return (
-        <Box m="0 5px 0 5px" height="100vh" overflow="auto">
+    <Box>
       {/* HEADER */}
-      <Box 
-      display="flex" 
-      justifyContent="center"
-       alignItems="center"
-       textAlign="center"
-       >
-        <Header title="Air Quality" subtitle="Monitoring the Air Quality" />
-      </Box>
-      <Box>
-        
-      <Box 
-            display="flex" 
-            // justifyContent="space-between"
-            //  alignItems="space-between"
-             sx={{
-              flexDirection: { xs: 'column', sm: 'row', md: "column", lg: "row" }
-            }}>
-      {/* <DBRecords/> */}
-      <AirQualityRecordTable/>
-
+          <Box 
+          display="flex" 
+          justifyContent="center"
+          alignItems="center"
+          textAlign="center"
+          overflow="auto"
+          >
+            <Header title="Heat Index" subtitle="Monitoring the Heat Index" />
+          </Box>
+        <Box sx={{ pl: 2 }}>
+        <Grid container>
+          <Grid item xs={12} >
+            <Box width="620px">
+            {/* <Box m="0 5px 0 5px" height="100vh" width="750px"
+            // overflow="auto"
+            > */}
+              <CO2Cards/>  
+            </Box> 
+          </Grid>
+        <Grid item xs={12} >
+          <Box width='500px' sx={{ pl: 2 }}>
+              <AirQualityRecordTable/>
+              <Card width="300px">
+                <CardMedia component='img' height='240vh'
+                image="../../../assets/iaq.png" alt='img'/>
+                
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Ideal IAQ
+                  </Typography>
+                  <Typography variant='body2' color="red">
+                    The ideal IAQ range is 1-5. <br/>
+                    Status: Good / Bad. <br/>
+                    Formula:
+                  </Typography>
+                </CardContent>
+                {/* dapat ma redirect sa faq page --> learn more button */}
+                <CardActions>
+                  <Button size='small'>Learn more</Button>
+                </CardActions>
+              </Card>
+            </Box>
+          </Grid>
+        </Grid>
         </Box>
-      <CO2Cards/>
-      </Box>
-      
-      
-    </Box> 
-  )
-}
+    </Box>
+      )
+    }
 
 export default CarbonDioxideMonitor;
